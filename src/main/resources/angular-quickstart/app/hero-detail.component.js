605347499564ec200b6f30549a501a2cde4380ee
Eugene <ernestyj@outlook.com>
2016-11-09 14:05:46 +0800

    u

--- src/main/resources/angular-quickstart/app/hero-detail.component.js
+++ src/main/resources/angular-quickstart/app/hero-detail.component.js
@@ -5,13 +5,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-var core_1 = require("@angular/core");
-var router_1 = require("@angular/router");
-var common_1 = require("@angular/common");
-var hero_service_1 = require("./hero.service");
+var core_1 = require('@angular/core');
 var HeroDetailComponent = (function () {
     function HeroDetailComponent(heroService, route, location) {
         this.heroService = heroService;
@@ -28,18 +22,14 @@ var HeroDetailComponent = (function () {
     HeroDetailComponent.prototype.goBack = function () {
         this.location.back();
     };
+    HeroDetailComponent = __decorate([
+        core_1.Component({
+            moduleId: module.id,
+            selector: 'my-hero-detail',
+            templateUrl: 'hero-detail.component.html',
+            styleUrls: ['hero-detail.component.css']
+        })
+    ], HeroDetailComponent);
     return HeroDetailComponent;
 }());
-HeroDetailComponent = __decorate([
-    core_1.Component({
-        moduleId: module.id,
-        selector: 'my-hero-detail',
-        templateUrl: 'hero-detail.component.html',
-        styleUrls: ['hero-detail.component.css']
-    }),
-    __metadata("design:paramtypes", [hero_service_1.HeroService,
-        router_1.ActivatedRoute,
-        common_1.Location])
-], HeroDetailComponent);
 exports.HeroDetailComponent = HeroDetailComponent;
-//# sourceMappingURL=hero-detail.component.js.map
